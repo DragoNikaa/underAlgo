399ea36763f9f8543bffa0a756cb1e505e03489f
Julia Jurczak <julia.jurczak15@gmail.com>
2025-07-22 14:48:21 +0200

    feat: add test case selection and change fetch method to post

--- algorithms/static/algorithms/scripts/visualization.js
+++ algorithms/static/algorithms/scripts/visualization.js
@@ -1,17 +1,54 @@
-document.getElementById("step-button").addEventListener("click", function () {
-	fetch(window.location.href, {
+document.getElementById("start-button").addEventListener("click", function () {
+	const selectedTestCase = document.querySelector("input[name=test-case]:checked");
+	if (!selectedTestCase) {
+		alert("Choose a test case!");
+		return;
+	}
+	fetch(getFetchUrl("start"), {
+			method: "POST",
 			headers: {
-				"X-Requested-With": "XMLHttpRequest",
-			}
+				"X-CSRFToken": getCSRFToken(),
+			},
+			body: JSON.stringify({
+				test_case_id: selectedTestCase.value,
+			})
 		})
 		.then(response => response.json())
 		.then(data => {
 			updateKeyValuePairs("input", data.input);
 			updateKeyValuePairs("variables", data.step.variables);
-			updateOutputAndButton(data.step.output);
+			hideElements("test-cases", "start-button");
+			showElements("next-step-button", "restart-button");
 			updateActiveLine(data.step.line);
 		});
 });
+
+document.getElementById("next-step-button").addEventListener("click", function () {
+	fetch(getFetchUrl("next-step"), {
+			method: "POST",
+			headers: {
+				"X-CSRFToken": getCSRFToken(),
+			},
+		})
+		.then(response => response.json())
+		.then(data => {
+			updateKeyValuePairs("variables", data.step.variables);
+			const output = data.step.output;
+			if (output) {
+				updateOutput(output);
+				disableButton("next-step-button");
+			}
+			updateActiveLine(data.step.line);
+		});
+});
+
+function getCSRFToken() {
+	return document.querySelector("[name=csrfmiddlewaretoken]").value;
+}
+
+function getFetchUrl(pathSegment) {
+	return window.location.href + pathSegment;
+}
 
 function updateKeyValuePairs(containerId, keyValueMap) {
 	const container = document.getElementById(containerId);
@@ -29,17 +66,14 @@ function createKeyValueElement(key, value) {
 	return element;
 }
 
-function updateOutputAndButton(output) {
+function updateOutput(output) {
 	const outputElement = document.getElementById("output");
-	const stepButton = document.getElementById("step-button");
+	outputElement.textContent = `output = ${output}`;
+}
 
-	if (output) {
-		outputElement.textContent = `output = ${output}`;
-		stepButton.textContent = "restart";
-	} else {
-		outputElement.textContent = "";
-		stepButton.textContent = "next";
-	}
+function disableButton(buttonId) {
+	button = document.getElementById(buttonId);
+	button.disabled = true;
 }
 
 function updateActiveLine(lineNumber) {
@@ -52,5 +86,19 @@ function removeActiveLine() {
 	const activeLine = document.querySelector("#lines .active-line");
 	if (activeLine) {
 		activeLine.classList.remove("active-line");
+	}
+}
+
+function hideElements(...elementIds) {
+	for (const elementId of elementIds) {
+		const element = document.getElementById(elementId);
+		element.classList.add("hidden");
+	}
+}
+
+function showElements(...elementIds) {
+	for (const elementId of elementIds) {
+		const element = document.getElementById(elementId);
+		element.classList.remove("hidden");
 	}
 }
--- algorithms/views.py
+++ algorithms/views.py
@@ -2,11 +2,11 @@ import json
 from typing import Any
 
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 
 from algorithms import algorithms_steps
-from algorithms.models import Algorithm
+from algorithms.models import Algorithm, TestCase
 
 
 class AlgorithmsView(View):
@@ -20,44 +20,6 @@ class VisualizationView(View):
     TEMPLATE_NAME = "algorithms/visualization.html"
 
     def get(self, request: HttpRequest, algorithm_name: str) -> HttpResponse:
-        if self._is_ajax_request(request):
-            return self._handle_next_algorithm_step(request, algorithm_name)
-        self._delete_algorithm_session(request, algorithm_name)
-        return self._render_algorithm_view(request, algorithm_name)
-
-    @staticmethod
-    def _is_ajax_request(request: HttpRequest) -> bool:
-        return request.headers.get("X-Requested-With") == "XMLHttpRequest"
-
-    def _handle_next_algorithm_step(self, request: HttpRequest, algorithm_name: str) -> HttpResponse:
-        algorithm_input, steps = self._get_algorithm_input_and_steps(request, algorithm_name)
-        next_step = steps.pop(0)
-        self._update_algorithm_session(request, algorithm_name, algorithm_input, steps)
-        return JsonResponse({"input": algorithm_input, "step": next_step})
-
-    @staticmethod
-    def _get_algorithm_input_and_steps(request: HttpRequest, algorithm_name: str) -> tuple[
-        dict[str, Any], list[dict[str, Any]]]:
-        try:
-            algorithm_data = request.session[algorithm_name]
-            algorithm_input, steps = algorithm_data["input"], algorithm_data["steps"]
-        except KeyError:
-            algorithm_input = {"numbers": [-2, 1, 3, 5, 6, 8, 13, 14], "target": 1}
-            steps = algorithms_steps.BinarySearch(**algorithm_input).get_steps()
-        return algorithm_input, steps
-
-    def _update_algorithm_session(self, request: HttpRequest, algorithm_name: str, algorithm_input: dict[str, Any],
-                                  steps: list[dict[str, Any]]) -> None:
-        if steps:
-            request.session[algorithm_name] = {"input": algorithm_input, "steps": steps}
-        else:
-            self._delete_algorithm_session(request, algorithm_name)
-
-    @staticmethod
-    def _delete_algorithm_session(request: HttpRequest, algorithm_name: str) -> None:
-        request.session.pop(algorithm_name, None)
-
-    def _render_algorithm_view(self, request: HttpRequest, algorithm_name: str) -> HttpResponse:
         algorithm = self._get_algorithm_from_db(algorithm_name)
         context = self._get_algorithm_data(algorithm)
         return render(request, self.TEMPLATE_NAME, context)
@@ -65,9 +27,55 @@ class VisualizationView(View):
     @staticmethod
     def _get_algorithm_from_db(algorithm_name: str) -> Algorithm:
         unslugged_algorithm_name = algorithm_name.replace("-", " ")
-        return Algorithm.objects.get(name=unslugged_algorithm_name)
+        return get_object_or_404(Algorithm, name=unslugged_algorithm_name)
 
     @staticmethod
     def _get_algorithm_data(algorithm: Algorithm) -> dict[str, Any]:
-        code = json.loads(algorithm.code)
-        return {"name": algorithm.name, "code": code}
+        test_cases = algorithm.testcase_set.all()
+        return {"name": algorithm.name, "code": algorithm.code, "test_cases": test_cases}
+
+
+class VisualizationStartView(View):
+    def post(self, request: HttpRequest, algorithm_name: str) -> HttpResponse:
+        algorithm_input = self._get_algorithm_input(request)
+        steps = self._get_initial_algorithm_steps(algorithm_input)
+        first_step = steps.pop(0)
+        self._create_algorithm_session(request, algorithm_name, algorithm_input, steps)
+        return JsonResponse({"input": algorithm_input, "step": first_step})
+
+    def _get_algorithm_input(self, request: HttpRequest) -> dict[str, Any]:
+        test_case = self._get_test_case(request)
+        algorithm_input: dict[str, Any] = test_case.body
+        return algorithm_input
+
+    @staticmethod
+    def _get_test_case(request: HttpRequest) -> TestCase:
+        test_case_id = json.loads(request.body)["test_case_id"]
+        return TestCase.objects.get(id=test_case_id)
+
+    @staticmethod
+    def _get_initial_algorithm_steps(algorithm_input: dict[str, Any]) -> list[dict[str, Any]]:
+        return algorithms_steps.BinarySearch(**algorithm_input).get_steps()
+
+    @staticmethod
+    def _create_algorithm_session(request: HttpRequest, algorithm_name: str, algorithm_input: dict[str, Any],
+                                  steps: list[dict[str, Any]]) -> None:
+        request.session[algorithm_name] = {"input": algorithm_input, "steps": steps}
+
+
+class VisualizationNextStepView(View):
+    def post(self, request: HttpRequest, algorithm_name: str) -> HttpResponse:
+        steps = self._get_algorithm_steps_from_session(request, algorithm_name)
+        next_step = steps.pop(0)
+        self._update_algorithm_session(request, algorithm_name, steps)
+        return JsonResponse({"step": next_step})
+
+    @staticmethod
+    def _get_algorithm_steps_from_session(request: HttpRequest, algorithm_name: str) -> list[dict[str, Any]]:
+        steps: list[dict[str, Any]] = request.session[algorithm_name]["steps"]
+        return steps
+
+    @staticmethod
+    def _update_algorithm_session(request: HttpRequest, algorithm_name: str, steps: list[dict[str, Any]]) -> None:
+        algorithm_input = request.session[algorithm_name]["input"]
+        request.session[algorithm_name] = {"input": algorithm_input, "steps": steps}

c7b4d7b0c66996ac8efdfaa708dd0a59efb992f9
Julia Jurczak <julia.jurczak15@gmail.com>
2025-08-26 01:58:14 +0200

    refactor: wrap test case selection in form and improve fetch handling

--- algorithms/static/algorithms/scripts/visualization.js
+++ algorithms/static/algorithms/scripts/visualization.js
@@ -1,75 +1,63 @@
-document.getElementById("start-button").addEventListener("click", handleStartButtonClick);
-document.getElementById("next-step-button").addEventListener("click", handleNextStepButtonClick);
-document.getElementById("restart-button").addEventListener("click", () => window.location.reload());
+addListeners();
 
-async function handleStartButtonClick() {
+function addListeners() {
+	document.getElementById("start-form").addEventListener("submit", handleStartFormSubmit);
+	document.getElementById("next-step-button").addEventListener("click", handleNextStepButtonClick);
+	document.getElementById("restart-button").addEventListener("click", () => window.location.reload());
+}
+
+async function handleStartFormSubmit(event) {
+	event.preventDefault();
 	const testCaseData = getSelectedTestCaseData();
 	if (!testCaseData) return;
-	try {
-		const data = await sendStartRequest(testCaseData);
-		if (!data) return;
-		hideElements("test-cases");
-		showElements("animation", "explanation");
-		prepareAnimationDisplay(data.input);
-		await handleAnimationStep(data.step);
-		updateActiveLine(data.step.line);
-	} catch (error) {
-		console.error(error);
-		alert("Something went wrong. Please try again.");
-	}
+	const data = await sendRequest("start", {
+		"test_case": testCaseData,
+	});
+	if (!data) return;
+	hideElements("test-cases");
+	showElements("animation", "explanation");
+	prepareAnimationDisplay(data.input);
+	await handleAnimationStep(data.step);
+	updateActiveLine(data.step.line);
 }
 
 async function handleNextStepButtonClick() {
+	const data = await sendRequest("next-step");
+	await handleAnimationStep(data.step);
+	const output = data.step.output;
+	if (output !== null) {
+		updateOutput(output);
+		disableButton("next-step-button");
+	}
+	updateActiveLine(data.step.line);
+}
+
+async function sendRequest(endpoint, bodyData = null) {
+	const options = getRequestOptions(bodyData);
 	try {
-		const data = await sendNextStepRequest();
-		await handleAnimationStep(data.step);
-		const output = data.step.output;
-		if (output !== null) {
-			updateOutput(output);
-			disableButton("next-step-button");
-		}
-		updateActiveLine(data.step.line);
+		const response = await fetch(endpoint, options);
+		const data = await response.json();
+		if (response.ok) return data;
+		if (response.status === 422)
+			data.errors.forEach(error =>
+				handleValidationError(error.field, error.input, error.message)
+			);
 	} catch (error) {
-		console.error(error);
 		alert("Something went wrong. Please try again.");
 	}
 }
 
-function sendStartRequest(testCaseData) {
-	const endpoint = getEndpoint("start");
-	const bodyData = {
-		"test_case": testCaseData,
-	};
-	return sendRequest(endpoint, bodyData);
-}
-
-function sendNextStepRequest() {
-	const endpoint = getEndpoint("next-step");
-	return sendRequest(endpoint);
-}
-
-async function sendRequest(endpoint, bodyData = null) {
+function getRequestOptions(bodyData) {
 	const options = {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
-			"X-CSRFToken": getCSRFToken(),
+			"Accept": "application/json",
+			"X-CSRFToken": document.querySelector("[name=csrfmiddlewaretoken]").value,
 		},
 	};
 	if (bodyData) options.body = JSON.stringify(bodyData);
-	const response = await fetch(endpoint, options);
-	const data = await response.json();
-	if (response.status === 422)
-		data.errors.forEach(error => handleValidationError(error.field, error.input, error.message));
-	else return data;
-}
-
-function getCSRFToken() {
-	return document.querySelector("[name=csrfmiddlewaretoken]").value;
-}
-
-function getEndpoint(pathSegment) {
-	return window.location.href + pathSegment;
+	return options;
 }
 
 function getSelectedTestCaseData() {
@@ -100,9 +88,9 @@ function getSelectedTestCase() {
 function getCustomTestCaseBody() {
 	const body = {};
 	let isInputValid = true;
-	const customInputs = document.querySelectorAll(".custom-input");
+	const customInputs = document.querySelectorAll("input[name=custom-input]");
 	customInputs.forEach(input => {
-		const fieldName = input.id.replace("-input", "");
+		const fieldName = input.id.replace("-custom", "");
 		try {
 			body[fieldName] = JSON.parse(input.value);
 			clearValidationError(fieldName);
@@ -116,14 +104,14 @@ function getCustomTestCaseBody() {
 
 function handleValidationError(fieldName, inputValue, userMessage, devMessage = null) {
 	if (!devMessage) devMessage = userMessage;
-	addClassToElements("invalid-input", `${fieldName}-input`);
+	addClassToElements("invalid-input", `${fieldName}-custom`);
 	displayTextInElement(`${fieldName}-error-message`, userMessage);
 	showElements(`${fieldName}-error-row`);
 	console.error(`Invalid value in input "${fieldName}": "${inputValue}".\nReason: "${devMessage}".`);
 }
 
 function clearValidationError(fieldName) {
-	removeClassFromElements("invalid-input", `${fieldName}-input`);
+	removeClassFromElements("invalid-input", `${fieldName}-custom`);
 	hideElements(`${fieldName}-error-row`);
 }
 

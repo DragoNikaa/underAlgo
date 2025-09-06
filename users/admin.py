f62efaca6060c9089e2905252d800ca17d1f128c
Julia Jurczak <julia.jurczak15@gmail.com>
2025-09-06 22:30:53 +0200

    feat: add custom user model with required email field

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users import models
+
+admin.site.register(
+    [
+        models.User,
+    ]
+)
--- users/views.py
+++ users/views.py
@@ -1,19 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import Any, TypeAlias, cast
+from typing import Any
 
 from django.contrib.auth import login, logout
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
 
-_UserRegistrationForm: TypeAlias = "UserCreationForm[User]"
-_UserForm: TypeAlias = "_UserRegistrationForm | AuthenticationForm"
+from users.forms import CustomUserCreationForm
+from users.models import User
 
 
 class _RegistrationLoginBaseView(View, ABC):
+    _UserForm = CustomUserCreationForm | AuthenticationForm
     FORM_FIELD_ID = "%s-input"
 
     @property
@@ -23,7 +22,7 @@ class _RegistrationLoginBaseView(View, ABC):
 
     @staticmethod
     @abstractmethod
-    def _get_authenticated_user(request: HttpRequest, form: Any) -> AbstractBaseUser:
+    def _get_authenticated_user(request: HttpRequest, form: Any) -> User:
         ...
 
     def base_get(self, request: HttpRequest, form: _UserForm) -> HttpResponse:
@@ -41,22 +40,22 @@ class _RegistrationLoginBaseView(View, ABC):
 
     def _get_and_login_user(self, request: HttpRequest, form: _UserForm) -> None:
         user = self._get_authenticated_user(request, form)
-        login(request, cast(User, user))
+        login(request, user)
 
 
 class RegistrationView(_RegistrationLoginBaseView):
     template_name = "users/registration.html"
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        form: _UserRegistrationForm = UserCreationForm(auto_id=self.FORM_FIELD_ID)
+        form = CustomUserCreationForm(auto_id=self.FORM_FIELD_ID)
         return self.base_get(request, form)
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        form: _UserRegistrationForm = UserCreationForm(request.POST, auto_id=self.FORM_FIELD_ID)
+        form = CustomUserCreationForm(request.POST, auto_id=self.FORM_FIELD_ID)
         return self.base_post(request, form)
 
     @staticmethod
-    def _get_authenticated_user(request: HttpRequest, form: _UserRegistrationForm) -> AbstractBaseUser:
+    def _get_authenticated_user(request: HttpRequest, form: CustomUserCreationForm) -> User:
         return form.save()
 
 
@@ -72,7 +71,7 @@ class LoginView(_RegistrationLoginBaseView):
         return self.base_post(request, form)
 
     @staticmethod
-    def _get_authenticated_user(request: HttpRequest, form: AuthenticationForm) -> AbstractBaseUser:
+    def _get_authenticated_user(request: HttpRequest, form: AuthenticationForm) -> User:
         return form.get_user()
 
 

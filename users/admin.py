583e070bc2bed166adea75d451d3135858ddcc0b
DragoNikaa <julia.jurczak15@gmail.com>
2026-07-03 18:34:54 +0200

    chore: init

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from users import models
-
-admin.site.register(
-    [
-        models.User,
-    ]
-)
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from users import views
-
-urlpatterns = [
-    path("register/", views.RegistrationView.as_view(), name="registration"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
-]
